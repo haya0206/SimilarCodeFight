e826b0330a0fb9000906a2b68fe17528a6a8a528
KimYoungHa <haya0206333@gmail.com>
2018-02-09 20:35:02 +0900

    r
    fsd

--- CodeFight/CodeFight/Form2.Designer.cs
+++ CodeFight/CodeFight/Form2.Designer.cs
@@ -70,7 +70,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(587, 354);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Visible = false;
             // 
             // user1
             // 
